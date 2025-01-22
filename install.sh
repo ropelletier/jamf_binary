c3d78af5f24389c0992d171b607c763d2bb90b68
ropelletier <88781432+ropelletier@users.noreply.github.com>
2025-01-22 13:38:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+sudo jamf removeFramework
 cd /tmp
 curl https://github.com/ropelletier/jamf_binary/raw/refs/heads/main/jamf.gz -o jamf.gz
 gunzip -fq /tmp/jamf.gz /usr/local/jamf/bin/jamf 
 sudo chmod +x /usr/local/jamf/bin/jamf 
+
+sudo ./jamf createConf -url https://rsu87.jamfcloud.com
+sudo jamf enroll -prompt
