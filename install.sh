67ac96e6db23a5ab937bfe5bf5c0d39558fd70bf
ropelletier <88781432+ropelletier@users.noreply.github.com>
2025-01-22 13:07:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+cd /tmp
+curl https://github.com/ropelletier/jamf_binary/raw/refs/heads/main/jamf.gz -o jamf.gz
+gunzip -fq /tmp/jamf.gz /usr/local/jamf/bin/jamf 
+sudo chmod +x /usr/local/jamf/bin/jamf 
