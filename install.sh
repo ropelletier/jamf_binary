d366fa63c50ea738b70208b556ed6efd224561a4
ropelletier <88781432+ropelletier@users.noreply.github.com>
2025-01-22 14:38:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,11 @@ sudo chmod +x /usr/local/jamf/bin/jamf
 
 sudo ./jamf createConf -url https://rsu87.jamfcloud.com
 sudo jamf enroll -prompt
+
+
+
+
+#sudo -u rhesseltine sudo profiles renew -type enrollment
+#https://grahamrpugh.com/2020/02/21/resetting-dep-without-reinstalling.html
+https://grahamrpugh.com/2020/02/21/resetting-dep-without-reinstalling.html
+https://nwstrauss.com/posts/2020-08-11-mitigating-mac-enrollment-failures/
