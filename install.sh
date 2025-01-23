cd4affdaabc313ad9dafee5fe0a53bc6c49526cc
ropelletier <88781432+ropelletier@users.noreply.github.com>
2025-01-23 10:01:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo jamf removeFramework
 cd /tmp
-curl https://github.com/ropelletier/jamf_binary/raw/refs/heads/main/jamf.gz -o jamf.gz
-gunzip -fq /tmp/jamf.gz /usr/local/jamf/bin/jamf 
-sudo chmod +x /usr/local/jamf/bin/jamf 
 
+#Reinstall jamf
+curl -OL https://github.com/ropelletier/jamf_binary/raw/refs/heads/main/jamf.gz
+gunzip -fq ./jamf.gz ~/jamf
+sudo chmod +x ~/jamf
+sudo mv ~/jamf /usr/local/jamf/bin/jamf 
 sudo ./jamf createConf -url https://rsu87.jamfcloud.com
 sudo jamf enroll -prompt
 
@@ -15,3 +17,5 @@ sudo jamf enroll -prompt
 #https://grahamrpugh.com/2020/02/21/resetting-dep-without-reinstalling.html
 https://grahamrpugh.com/2020/02/21/resetting-dep-without-reinstalling.html
 https://nwstrauss.com/posts/2020-08-11-mitigating-mac-enrollment-failures/
+
+https://snelson.us/2022/08/jamf-binary-self-heal-via-terminal/
